95a465aaa1c3604e52e576991c052b7f08f41c9a
Paul Smith <paul@adhocteam.us>
2017-07-10 10:51:13 -0500

    Initial Commit

---
+++ CureAudiod/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CureAudiod
+//
+//  Created by Paul Smith on 7/10/17.
+//  Copyright © 2017 Paul Smith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
